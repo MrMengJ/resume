c605150b639c25c3e4111eb5126d6d83755e1c64
MrMengJ <2646973632@qq.com>
2017-04-18 01:12:14 +0800

    index.html秒杀区滑动 初步

--- Mobile-E-commerce-sites/js/main.js
+++ Mobile-E-commerce-sites/js/main.js
@@ -1,32 +1,39 @@
 $(document).ready(function () {
     search();
+    bannerMove()
     secondKill();
-    productLength();
+    productMove();
 })
-var banner = $(".banner"),  //获取最外层框架的名称
-    ul = banner.find("ul"),
-    showNumber = banner.find(".showNav li"),//获取按钮
-    oneWidth = banner.find("ul li").eq(0).width(); //获取每个图片的宽度
-var timer = null; //定时器返回值，主要用于关闭定时器
-var iNow = 0;  //iNow为正在展示的图片索引值，当用户打开网页时首先显示第一张图，即索引值为0
-showNumber.on("click", function () {   //为每个按钮绑定一个点击事件
-    $(this).addClass("active").siblings().removeClass("active"); //按钮点击时为这个按钮添加高亮状态，并且将其他按钮高亮状态去掉
-    var index = $(this).index(); //获取哪个按钮被点击，也就是找到被点击按钮的索引值
-    iNow = index;
 
-    ul.animate({
 
-        "left": -oneWidth * iNow, //注意此处用到left属性，所以ul的样式里面需要设置position: relative; 让ul左移N个图片大小的宽度，N根据被点击的按钮索引值iNOWx确定
-    })
-});
+//轮播图自动播放
+function bannerMove() {
+    var banner = $(".banner"),  //获取最外层框架的名称
+        ul = banner.find("ul"),
+        showNumber = banner.find(".showNav li"),//获取按钮
+        oneWidth = banner.find("ul li").eq(0).width(); //获取每个图片的宽度
+    var timer = null; //定时器返回值，主要用于关闭定时器
+    var iNow = 0;  //iNow为正在展示的图片索引值，当用户打开网页时首先显示第一张图，即索引值为0
+    showNumber.on("click", function () {   //为每个按钮绑定一个点击事件
+        $(this).addClass("active").siblings().removeClass("active"); //按钮点击时为这个按钮添加高亮状态，并且将其他按钮高亮状态去掉
+        var index = $(this).index(); //获取哪个按钮被点击，也就是找到被点击按钮的索引值
+        iNow = index;
 
-timer = setInterval(function () {  //打开定时器
-    iNow++;       //让图片的索引值次序加1，这样就可以实现顺序轮播图片
-    if (iNow > showNumber.length - 1) { //当到达最后一张图的时候，让iNow赋值为第一张图的索引值，轮播效果跳转到第一张图重新开始
-        iNow = 0;
-    }
-    showNumber.eq(iNow).trigger("click"); //模拟触发数字按钮的click
-}, 2500); //2000为轮播的时间
+        ul.animate({
+
+            "left": -oneWidth * iNow, //注意此处用到left属性，所以ul的样式里面需要设置position: relative; 让ul左移N个图片大小的宽度，N根据被点击的按钮索引值iNOWx确定
+        })
+    });
+
+    timer = setInterval(function () {  //打开定时器
+        iNow++;       //让图片的索引值次序加1，这样就可以实现顺序轮播图片
+        if (iNow > showNumber.length - 1) { //当到达最后一张图的时候，让iNow赋值为第一张图的索引值，轮播效果跳转到第一张图重新开始
+            iNow = 0;
+        }
+        showNumber.eq(iNow).trigger("click"); //模拟触发数字按钮的click
+    }, 2500); //2000为轮播的时间
+}
+
 
 
 // 搜索框固定，变色
@@ -76,31 +83,102 @@ var secondKill = function () {
 }
 
 // 计算秒杀区商品栏长度
-function productLength() {
-    var productNum = $('.product_bot li');
-    var productWidth = null;
-    //设置商品的宽度
-    productNum.width($('.product_bot').width()*0.3);
+// function productLength() {
+//     var productNum = $('.product_bot li');
+//     var productWidth = null;
+//     //设置商品的宽度
+//     productNum.width($('.product_bot').width()*0.3);
+//
+//     //遍历获取所有商品的总长度
+//     productNum.each(function (index,element) {
+//         productWidth += element.clientWidth;
+//     })
+//     // console.log(productWidth)
+//     // console.log($('.product_bot').width()*0.3)
+//     productNum.parent('ul').width(productWidth+10);
+//
+//     //设置product_wrap跟product_bot的高度
+//     // console.log($('.product_bot ul').height());
+//     // console.log($('.product_bot ul').height()+50);
+//     // $(".product_wrap").height($('.product_bot ul').height()+50);
+//     // $(".product_bot").height($('.product_wrap').height()-30);
+//     // console.log($('.product_bot ul').height());
+//     // // 这种方法返回undefined，为什么
+//     // for (var i = 0; i < productNum; i++) {
+//     //     console.log(parseFloat($('.product_bot li :eq(i)').width()))
+//     //     productWidth += $('.product_bot li :eq(i)').width();
+//     //     // console.log(productWidth)
+//     // }
+//
+// }
 
-    //遍历获取所有商品的总长度
-    productNum.each(function (index,element) {
-        productWidth += element.clientWidth;
+
+
+// 商品滑动
+function productMove() {
+    var parentDom = document.getElementsByClassName('product_bot')[0];//获取父盒子
+    var childDom =parentDom.getElementsByTagName('ul')[0];//获取子盒子
+    var liDom = $('.product_bot li');//li标签，商品列表
+    var liW = parentDom.offsetWidth*0.3;
+    liDom.width(liW);//单个商品的宽
+    var productWidth = 0;//所有商品总占用总宽度
+    // 遍历设置商品总宽度
+    liDom.each(function(index,element){
+        productWidth += element.offsetWidth;
     })
-    // console.log(productWidth)
-    // console.log($('.product_bot').width()*0.3)
-    productNum.parent('ul').width(productWidth+10);
+    productWidth += 30;
+    //获取父盒子宽度
+    var parentW = parentDom.offsetWidth;
 
-    //设置product_wrap跟product_bot的高度
-    // console.log($('.product_bot ul').height());
-    // console.log($('.product_bot ul').height()+50);
-    // $(".product_wrap").height($('.product_bot ul').height()+50);
-    // $(".product_bot").height($('.product_wrap').height()-30);
-    // console.log($('.product_bot ul').height());
-    // // 这种方法返回undefined，为什么
-    // for (var i = 0; i < productNum; i++) {
-    //     console.log(parseFloat($('.product_bot li :eq(i)').width()))
-    //     productWidth += $('.product_bot li :eq(i)').width();
-    //     // console.log(productWidth)
-    // }
+    //设置子盒子宽度
+    // childDom.offsetWidth = productWidth; //无效
+    // childDom.clientWidth = productWidth; //无效
+    $(childDom).width(productWidth);//有效  这是为何？？
+    var childW = childDom.offsetWidth;
 
+    var startX = 0;//开始时的X坐标
+    var endX = 0; //结束时的X坐标
+    var moveX = 0;//手指滑动的距离
+    var currentX = 0;//当前translateX值
+
+    // 限制最大、最小滑动距离
+    var minMoveX = -childW;
+    var maxMoveX = 0;
+    //touchstart 手指接触屏幕
+    childDom.addEventListener('touchstart',function(event){
+        startX = event.touches[0].clientX;//获取开始时的X坐标
+    },false)
+
+    //touchmove  手指滑动屏幕
+    childDom/addEventListener('touchmove',function (event) {
+        // event.preventDefault();
+        endX = event.touches[0].clientX; //获取结束时的X坐标
+        moveX = endX - startX;
+        if ((currentX+moveX) <= maxMoveX && (currentX+moveX) >= minMoveX){
+            childDom.style.webkitTransform = "translateX("+(currentX+moveX)+"px)";
+            childDom.style.mozTransform = "translateX("+(currentX+moveX)+"px)";
+            childDom.style.oTransform = "translateX("+(currentX+moveX)+"px)";
+            childDom.style.msTransform = "translateX("+(currentX+moveX)+"px)";
+            childDom.style.Transform = "translateX("+(currentX+moveX)+"px)";
+        }
+    },false)
+
+    //touchcannel touch事件意外中断
+    childDom.addEventListener('touchcannel',function () {
+        //记录当前的translateX值
+        currentX = moveX + currentX;
+    },false)
+
+    //touchend 手指离开屏幕
+    childDom.addEventListener('touchend',function () {
+        //记录当前的translateX值
+        if((currentX+moveX) >= -(childW - parentW) && (currentX+moveX) <= maxMoveX){
+            currentX = moveX + currentX;
+        }
+        //向左划过头就跳转到更多秒杀页面
+        else if((currentX + moveX) < -(childW - parentW)){
+            window.location = "goods.html"
+        }
+
+    },false)
 }
